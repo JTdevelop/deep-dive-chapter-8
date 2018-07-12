76dcef42b57be485fd2568708bba5a752e7eefc3
Jay T <torrcorp@gmail.com>
2018-07-11 20:47:28 -0600

    Initial commit

---
+++ src/edu/cnm/deepdive/ICharQ.java
@@ -0,0 +1,10 @@
+package edu.cnm.deepdive;
+
+// A character queue interface.
+public interface ICharQ {
+  // Put a character into the queue.
+  void put (char ch);
+
+  // get a character from the queue.
+  char get();
+}
